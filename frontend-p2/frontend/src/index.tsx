74a11f2897b338a9ec545c4a52e7ee4774e97d88
tyler <tkcraine@gmail.com>
2024-08-28 23:24:18 -0400

    broken

--- frontend-p2/frontend/src/index.tsx
+++ frontend-p2/frontend/src/index.tsx
@@ -12,6 +12,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <h1></h1>
   <React.StrictMode>
     <h1>lovely lovely lovely lovely lovely lovely lovely</h1>
     <BrowserRouter>
